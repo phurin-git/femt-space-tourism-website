7449001bb9a7edd19c74cd3c7b281b60f3402d9c
Phurin Nararat <phurin.nrt@gmail.com>
2024-08-09 12:26:37 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-900">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'blue': {
+        900: '#0B0D17',
+      300: 'D0D6F9'
+      },
+      'white': '#FFFFFF',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
