4a51e62360c805f92d97df269257da4d27fa7d95
Phurin Nararat <phurin.nrt@gmail.com>
2024-08-18 00:18:44 +0700

    Fix bug
    Fix: redirect script in 404.html

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import Home from './pages/Home'
 import Destination from './pages/Destination'
 import Crew from './pages/Crew'
 import Technology from './pages/Technology'
+import Error from './pages/Error'
 import './index.css'
 import hamburger from './assets/shared/icon-hamburger.svg'
 import close from './assets/shared/icon-close.svg'
@@ -83,11 +84,12 @@ export const App = () => {
               <Outlet />
             </div>
           }>
-            <Route path='/' element={<Home />}/>
+            <Route index element={<Home />}/>
             <Route path='/home' element={<Home />}/>
             <Route path='/destination' element={<Destination />}/>
             <Route path='/crew' element={<Crew />}/>
             <Route path='/technology' element={<Technology />}/>
+            <Route path="*" element={<Error />} />
           </Route>
       </Routes>
   )
